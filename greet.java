09a089ba17e35d1e226cd3b536916fe31658630d
VARUN SINGH <varunsinghrj4@gmail.com>
2024-06-13 11:16:35 +0530

    Add files via upload

---
+++ greet.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class greet {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter your name : ");
+       String name = s.next();
+        System.out.println(name +" hava a good day ");
+
+    }
+}
