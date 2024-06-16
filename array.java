e087aceb6c585e63b56469f0755d72bd326d7d9c
VARUN SINGH <varunsinghrj4@gmail.com>
2024-06-16 14:09:30 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,11 @@
+public class array {
+    public static void main(String[] args) {
+
+        int[] marks = {45, 75, 78, 69, 52};
+        for (int  element : marks) System.out.println(element);
+// 2d aaray
+        int [][] mr = {{1, 2,3},{4,5,6}};
+        System.out.println(mr[1][0]);
+
+    }
+}
