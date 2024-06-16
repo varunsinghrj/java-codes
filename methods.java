31467f4e2d9b9fa0c52efeadee99794d4f50ac34
VARUN SINGH <varunsinghrj4@gmail.com>
2024-06-16 14:09:48 +0530

    Add files via upload

---
+++ methods.java
@@ -0,0 +1,12 @@
+public class methods {
+
+    static int avg(int x,int y){
+        int z;
+      z= (x+y)/2;
+        return z;
+    }
+    public static void main(String[] args) {
+        int a = avg(4,4);
+        System.out.println("Average :"+ a);
+    }
+}
